1ca64db8e035fa0ac04b4d77f2d1e044d71129e1
rajivkr12 <128715119+rajivkr12@users.noreply.github.com>
2025-05-11 12:28:15 +0530

    Initial commit with CSV and SQL files tracked by Git LFS

---
+++ source_data/scripts/build_tables.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9c3b24e84b8d0adfc351f409b5fadf58670ee0ecc6cd2e109c035aebdc23f41
+size 6985
---
+++ source_data/scripts/chicago_crimes_and_weather.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63962de6cabeae63116ffec6094bdef14a715d25812c03a35a6b9d049aca8ede
+size 23807
